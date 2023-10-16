28f053949d91b79c71f4c49c0a1a36b3ed9222bf
lucas <macbookpro@MBP-de-MacBook>
2023-10-16 13:12:05 +0200

    now, when you launch the app, if you arleady had data, open the data file, and create a new one otherwise

---
+++ src/main.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+import os 
+import classNote as cNote
+import functions as fct 
+
+def main() :
+
+    # Functions used in the loop 
+    def createNewNote() :
+        fct.launchNewNoteWindow()
+
+    # Open data : 
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    dataFilePath = os.path.join(script_dir,"..", "data", "data.txt")    
+    if not os.path.exists(dataFilePath) :
+        print("The file is not existing yet. Creating new one ...")
+        with open(dataFilePath,"w") as file : 
+            file.write("")
+        
+    else : 
+        ("Opening your file ...")
+        fct.loadData(dataFilePath)
+        
+    # Creation of the main window 
+    window = tk.Tk()
+    window.title("My Notes")
+    window.geometry("720x700")
+
+    # Creation of the differents object of the app 
+    # Text field with all the notes
+
+
+    # Button for the New Note
+    buttonCreate = tk.Button(window, text="Create New Note", command=createNewNote,
+                                bg ="blue", fg="black",font=("Comic Sans MS",14))
+    buttonCreate.pack(side=tk.BOTTOM, pady=10)
+
+    # Last instruction
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
